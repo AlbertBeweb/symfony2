85f781a36cc1c9ca601db37bfb1dba1435709a95
AlbertBeweb <contact@albert-nicolas.fr>
2019-11-07 13:37:26 +0100

    Add Homepage with bootstrap

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,6 +14,12 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            //Permet d'avoir le link de la navbar en active
+            'current_menu' => 'home',
+            //Variable pour le titre et l'onglet
+            'title' => 'Home',
+            //Titre de l'application
+            'appName' => 'StarterKit Symfony 4'
         ]);
     }
 }
