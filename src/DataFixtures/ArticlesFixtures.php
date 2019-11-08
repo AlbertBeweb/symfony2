3c6967c22f4fe296e63d4b887524510be64c75f8
AlbertBeweb <contact@albert-nicolas.fr>
2019-11-08 18:43:36 +0100

    Create faker fixtures

--- src/DataFixtures/ArticlesFixtures.php
+++ src/DataFixtures/ArticlesFixtures.php
@@ -2,7 +2,10 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -10,16 +13,48 @@ class ArticlesFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for($i = 1; $i <= 10; $i++)
+        $faker = Faker\Factory::create('FR_fr');
+
+        for ($i = 1; $i <= 3; $i++)
         {
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                     ->setDescription($faker->paragraph());
+
+            $manager->persist($category);
+
+            $content =  '<p>' . join($faker->paragraphs(5), '</p><p>') . '</p>';
+
+        for ($j = 1; $j <= mt_rand(4, 6); $j++) {
             $article = new Article();
-            $article->setTitle("Titre de l'article n°$i")
-                    ->setContent("Contenu de l'article n°$i")
-                    ->setImage("https://via.placeholder.com/350x150/")
-                    ->setCreateAt(new \DateTime());
+            $article->setTitle($faker->sentence())
+                ->setContent($content)
+                ->setImage($faker->imageUrl())
+                ->setCreateAt($faker->dateTimeBetween('-6 months'))
+                ->setCategory($category);
 
             $manager->persist($article);
+
+        for($k = 1; $k <= mt_rand(4, 10); $k++)
+        {
+            $comment = new Comment();
+
+            $content =  '<p>' . join($faker->paragraphs(2), '</p><p>') . '</p>';
+
+            $days = (new \DateTime())->diff($article->getCreateAt())->days;
+
+            $comment->setAuthor($faker->name)
+                    ->setContent($content)
+                    ->setCreateAt($faker->dateTimeBetween('-' . $days . ' days'))
+                    ->setArticle($article);
+
+            $manager->persist($comment);
         }
-            $manager->flush();
+
+        }
+
+        }
+
+        $manager->flush();
     }
 }
